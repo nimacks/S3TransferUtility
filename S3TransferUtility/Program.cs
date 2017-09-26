7d64c3ed50c5a4ed047bb0eb3940c2ca3d4c5781
Samuel Mensah <samuelnmensah@live.com>
2017-09-26 09:27:23 -0700

    Add project files.

---
+++ S3TransferUtility/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3TransferUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
