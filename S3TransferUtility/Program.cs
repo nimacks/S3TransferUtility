d895034b2998bba318dfc42e59c49b59004c8839
Samuel Mensah <samuelnmensah@live.com>
2017-09-26 13:28:56 -0700

    added transferutility class

--- S3TransferUtility/Program.cs
+++ S3TransferUtility/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace S3TransferUtility
 {
@@ -10,6 +6,19 @@ namespace S3TransferUtility
     {
         static void Main(string[] args)
         {
+            var directoryToUpload = @"c:\\Dev\\site";
+            var bucketName = "s3mediatransfers/transfers/site";
+
+            //Upload Directory
+            S3AssetTransferUtility transferUtility = new S3AssetTransferUtility();
+            var uploadStatus = transferUtility.SaveAsset(directoryToUpload, bucketName);
+
+            Console.WriteLine(string.Format("Upload to S3 Succeded : {0}", uploadStatus));
+
+            //Delete Directory
+            var deleteStatus = transferUtility.DeleteAsset("s3mediatransfers", "transfers\\site");
+            Console.WriteLine(string.Format("Directory Deletion from S3 Succeded : {0}",deleteStatus));
+        
         }
     }
 }
---
+++ S3TransferUtility/S3AssetTransferUtility.cs
@@ -0,0 +1,92 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.IO;
+using Amazon.S3.Transfer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3TransferUtility
+{
+    public class S3AssetTransferUtility
+    {
+        private readonly TransferUtility _transferUtility;
+        private readonly AmazonS3Client _client;
+        public S3AssetTransferUtility()
+        {
+            _transferUtility = new TransferUtility("", "", RegionEndpoint.USWest2);
+            _client = new AmazonS3Client("", "", RegionEndpoint.USWest2);
+        }
+
+        /// <summary>
+        /// Upload specified Diretory to S3 bucket
+        /// </summary>
+        /// <param name="uploadDirectory"></param>
+        /// <param name="bucket"></param>
+        /// <returns></returns>
+        public bool SaveAsset(string uploadDirectory, string bucket)
+        {
+            try
+            {
+
+                TransferUtilityUploadDirectoryRequest request = new TransferUtilityUploadDirectoryRequest
+                {
+                    BucketName = bucket,
+                    Directory = uploadDirectory,
+                    SearchOption = System.IO.SearchOption.AllDirectories,
+                    CannedACL = S3CannedACL.PublicRead
+                };
+                _transferUtility.UploadDirectory(request);
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.InnerException.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Delete Directory from S3
+        /// </summary>
+        /// <param name="uploadDirectory"></param>
+        /// <param name="bucket"></param>
+        /// <returns></returns>
+        public bool DeleteAsset(string bucket, string uploadDirectory)
+        {
+            try
+            {
+                S3DirectoryInfo directoryToDelete = new S3DirectoryInfo(_client, bucket, uploadDirectory);
+
+                var directoryFiles = directoryToDelete.EnumerateFiles();
+                foreach (S3FileInfo file in directoryFiles)
+                {
+                    S3FileInfo filetoDelete = new S3FileInfo(_client, bucket, file.FullName.Replace(bucket + ":\\", string.Empty));
+                    if (filetoDelete.Exists)
+                    {
+                        filetoDelete.Delete();
+                    }
+                }
+
+
+                if (directoryToDelete.Exists)
+                {
+                    directoryToDelete.Delete(false);
+                    return true;
+                }
+                
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.InnerException.Message);
+
+                return false;
+            }
+            return false;
+        }
+
+    }
+}
